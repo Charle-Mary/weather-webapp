59c209ce3ac2246a9bfb89b05619145936b295bf
Akintunde Adebiyi <charlesmaryayo@gmail.com>
2022-03-11 00:32:16 +0100

    Added configurations for environment variables

--- set_env.sh
+++ set_env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export X_RAPIDAPI_HOST='foreca-weather.p.rapidapi.com'
-export X_RAPIDAPI_KEY='FILL IN YOUR KEY'
-export MAPBOX_ACCESS_TOKEN='FILL IN YOUR TOKEN'
+export X_RAPIDAPI_KEY='b515287a96msh5d85a8855aebb8fp1d25b9jsnf2f211ec4c7a'
+export MAPBOX_ACCESS_TOKEN='pk.eyJ1IjoiY2hhcmxpbWFyeSIsImEiOiJjbDBkdXA2aXEwY2RyM2RuMDk2cnR3dGplIn0.RKLEaolqWh0gXYoeU7CJIg&limit=1'
